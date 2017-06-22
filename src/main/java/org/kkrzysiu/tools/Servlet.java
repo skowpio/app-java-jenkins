03de21cd132efe05d8e95e0b1b831b52d26dda60
kkrzysiu <kpwkowalcz@gmail.com>
2017-06-22 23:43:22 +0200

    Code refactoring

---
+++ src/main/java/org/kkrzysiu/tools/Servlet.java
@@ -0,0 +1,41 @@
+package org.kkrzysiu.tools;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class Servlet {
+
+    public static void main(String[] args) throws LifecycleException,
+            InterruptedException, ServletException {
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(16384);
+
+        Context ctx = tomcat.addContext("/", new File(".").getAbsolutePath());
+
+        Tomcat.addServlet(ctx, "Embedded", new HttpServlet() {
+            @Override
+            protected void service(HttpServletRequest req, HttpServletResponse resp) 
+                    throws ServletException, IOException {
+                
+                Writer w = resp.getWriter();
+                w.write("This is an example application, version 1.0.\n");
+                w.flush();
+                w.close();
+            }
+        });
+
+        ctx.addServletMapping("/*", "Embedded");
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
