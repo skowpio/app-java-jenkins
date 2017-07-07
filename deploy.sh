09e39315d9d5f3b034d4324af15cbf80fe320b78
skowpio <skowpio@gmail.com>
2017-07-07 20:01:31 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ INSTALL_DIR="/opt/${APP_NAME}/custom_deploy/install_dir"
 WORK_DIR="/tmp"
 PACKAGE_PATH="${WORK_DIR}/${APP_PACKAGE}"
 ARCHIVE_DIR="/opt/${APP_NAME}/custom_deploy/archive"
+POM_VERSION_FILE="${INSTALL_DIR}/current/pom_version.txt"
 
 mkdir -p ${INSTALL_DIR}
 mkdir -p ${ARCHIVE_DIR}
@@ -32,6 +33,14 @@ if [ -L ${INSTALL_DIR}/current ]; then
 fi 
 
 ln -s ${INSTALL_DIR}/${VERSION} ${INSTALL_DIR}/current
+
+# unzip maven zip package
+if [ -f ${POM_VERSION_FILE} ]; then 
+  POM_VERSION=$(echo pom_version.txt)
+  unzip app-java-jenkins-${POM_VERSION}-dist.zip -d ${INSTALL_DIR}/current
+  mv ${INSTALL_DIR}/current/app-java-jenkins-${POM_VERSION}/*.jar ${INSTALL_DIR}/current
+  rm -fr ${INSTALL_DIR}/current/app-java-jenkins-${POM_VERSION}
+fi
 
 supervisorctl start ${APP_NAME}-custom-deploy
 mv ${PACKAGE_PATH} ${ARCHIVE_DIR}
