b329aed00ccc542320e19db65f713fa0739ed32b
kkrzysiu <kpwkowalcz@gmail.com>
2017-07-10 22:09:49 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -2,6 +2,9 @@
 
 if [ -f /opt/app-jenkins/app-jenkins.pid ]
 then
+if [ $(ps -o pid h $(cat /opt/app-jenkins/app-jenkins.pid) | wc -l) == 1 ]
+then
 kill $(cat /opt/app-jenkins/app-jenkins.pid)
+fi
 rm -rf /opt/app-jenkins/app-jenkins.pid
 fi
