3c183bd5c2424353c715b162511f75e7e9531caa
kkrzysiu <kpwkowalcz@gmail.com>
2017-07-11 00:27:49 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/opt/java/jdk1.8.0_131/bin/java -cp $(JARS=(/opt/app-jenkins/custom_deploy/install_dir/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=Jenkins org.kkrzysiu.tools.Servlet 1>/dev/null 2>&1 &
+/opt/java/jdk1.8.0_131/bin/java -cp $(JARS=(/opt/app-jenkins/install/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=$1 org.kkrzysiu.tools.Servlet 1>/dev/null 2>&1 &
 echo $! > /opt/app-jenkins/app-jenkins.pid
