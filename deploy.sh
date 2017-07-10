25e12a695ee0e6e18ca6522b0ed68a3e63eec474
kkrzysiu <kpwkowalcz@gmail.com>
2017-07-10 20:28:27 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,46 +1,16 @@
-#!/bin/bash 
+#!/bin/bash
 
-get_help() {
-  echo "Usage:\n $0 app-jenkins-1.tar.gz"
-}
-
-if [ $# -eq 0 ]; then 
-  get_help
-  exit 1
-fi 
-
-APP_PACKAGE=${1}
-VERSION=$(echo ${APP_PACKAGE} | awk -F'-' '{print $3}' | tr -d 'tar.gz')
-APP_NAME=$(echo ${APP_PACKAGE} | awk -F'-' '{print $1"-"$2}')
-
-INSTALL_DIR="/opt/${APP_NAME}/custom_deploy/install_dir"
-WORK_DIR="/tmp"
-PACKAGE_PATH="${WORK_DIR}/${APP_PACKAGE}"
-ARCHIVE_DIR="/opt/${APP_NAME}/custom_deploy/archive"
-POM_VERSION_FILE="${INSTALL_DIR}/current/pom_version.txt"
+INSTALL_DIR="/opt/app-jenkins/custom_deploy/install_dir"
+WORK_DIR="/tmp/target"
+POM_VERSION=$(cat $WORK_DIR/app-java-jenkins-version.properties)
+PACKAGE_PATH="${WORK_DIR}/app-java-jenkins-$POM_VERSION-dist.zip"
 
 mkdir -p ${INSTALL_DIR}
-mkdir -p ${ARCHIVE_DIR}
 
-supervisorctl stop ${APP_NAME}-custom-deploy
+#supervisorctl stop app-jenkins-custom-deploy
 
-if [ -f ${PACKAGE_PATH} ]; then 
-  tar zxf "${PACKAGE_PATH}" -C "${INSTALL_DIR}"
-fi
+unzip -q -o ${PACKAGE_PATH} -d ${INSTALL_DIR}
 
-if [ -L ${INSTALL_DIR}/current ]; then 
-  rm -f ${INSTALL_DIR}/current
-fi 
+ln -fs ${INSTALL_DIR}/app-java-jenkins-${POM_VERSION} ${INSTALL_DIR}/current
 
-ln -s ${INSTALL_DIR}/${VERSION} ${INSTALL_DIR}/current
-
-# unzip maven zip package
-if [ -f ${POM_VERSION_FILE} ]; then 
-  POM_VERSION=$(cat ${POM_VERSION_FILE})
-  unzip ${INSTALL_DIR}/current/app-java-jenkins-${POM_VERSION}-dist.zip -d ${INSTALL_DIR}/current
-  mv ${INSTALL_DIR}/current/app-java-jenkins-${POM_VERSION}/*.jar ${INSTALL_DIR}/current
-  rm -fr ${INSTALL_DIR}/current/app-java-jenkins-${POM_VERSION}
-fi
-
-supervisorctl start ${APP_NAME}-custom-deploy
-mv ${PACKAGE_PATH} ${ARCHIVE_DIR}
+#supervisorctl start app-jenkins-custom-deploy
