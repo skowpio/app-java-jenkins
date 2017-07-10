c703c28e8885a6b6141c25bcfd72bbdcae16c4e9
kkrzysiu <kpwkowalcz@gmail.com>
2017-07-10 21:40:41 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-$JAVA_HOME/bin/java -cp $(JARS=(/opt/app-jenkins/custom_deploy/install_dir/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=Jenkins org.kkrzysiu.tools.Servlet
+$JAVA_HOME/bin/java -cp $(JARS=(/opt/app-jenkins/custom_deploy/install_dir/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=Jenkins org.kkrzysiu.tools.Servlet 1>/dev/null 2>&1 &
+echo $! > /opt/app-jenkins/app-jenkins.pid
