8d4c56638d71fdcc86fd8f00aa79c865d1d6f212
skowpio <skowpio@gmail.com>
2017-07-13 09:28:41 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-/opt/java/jdk1.8.0_131/bin/java -cp $(JARS=(/opt/app-jenkins/install/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=$1 org.kkrzysiu.tools.Servlet 1>/dev/null 2>&1 &
-echo $! > /opt/app-jenkins/app-jenkins.pid
+is_run=$(ps x | grep -v grep | grep -c java)
+
+if [ $is_run -eq 0 ]; then
+        /opt/java/jdk1.8.0_131/bin/java -cp $(JARS=(/opt/app-jenkins/install/current/*.jar); IFS=:; echo "${JARS[*]}") -DdeployedBy=$1 org.kkrzysiu.tools.Servlet 1>/dev/null 2>&1 &
+        echo $! > /opt/app-jenkins/app-jenkins.pid
+fi
